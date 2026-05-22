7080f4aa894e94854a17ce2c89caced474738630
Alexis Carreras <alexiscarreras@hotmail.com>
2026-05-22 00:12:39 -0300

    feat(home): agrega landing en construccion con contador hasta el 5 de junio

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,3 +1,5 @@
+import { UnderConstruction } from '@/features/under-construction/UnderConstruction';
+
 export const HomePage = () => {
-  return <div>HomePage</div>;
+  return <UnderConstruction />;
 };
